df0d7af2f09dfb1890059fac29627fcda9561b24
Letargikus-oposszum <144096062+Letargikus-oposszum@users.noreply.github.com>
2025-01-19 10:57:34 +0100

    Update WastePoint.cs

--- Database/WastePoint.cs
+++ Database/WastePoint.cs
@@ -5,6 +5,7 @@ public class WastePoint
     [DatabaseGenerated(DatabaseGeneratedOption.None)]
     public int Id { get; set; }
     public string Name { get; set; }
+    public string Category {get; set;}
     public float Latitude { get; set; }
     public float Longitude { get; set; }
     public string Address { get; set; }
@@ -14,6 +15,7 @@ public class ExternalWastePointModel
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string Category {get; set;}
     public float Latitude { get; set; }
     public float Longitude { get; set; }
     public string Address { get; set; }
